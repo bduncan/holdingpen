f696229a6416e0f29191c1a75324c9b82f0ffd64
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-03-08 21:36:42 +0000

    Really fix exec arguments!
    Clean up the code and add some comments.

--- gatekeeper.py
+++ gatekeeper.py
@@ -9,21 +9,20 @@ import subprocess
 import atexit
 
 
-def cleanup(sock):
-    sock.close()
-
-
 def main():
     defaults = {"socket": "/var/run/holdingpen.socket"}
     config = ConfigParser.SafeConfigParser(defaults)
     config.read('/etc/holdingpen.conf')
     sock = socket.socket(socket.AF_UNIX)
     sock.connect(config.get("main", "socket"))
-    os.execvp(sys.argv[0], sys.argv)
-    #atexit.register(cleanup, sock)
-    # close_fds is the default, but since we rely on it strongly, make it
-    # explicit.
-    #subprocess.Popen(sys.argv, close_fds=False)
+    # Pass the opened socket to the program. If the program decides to close
+    # all descriptors, this will not work. An alternative will be fork and
+    # pause for SIGCHLD before exiting.
+    # Again, that won't work if the program forks and execs. An excessive
+    # alternative in that case would be to use ptrace.
+    # In any case, the socket should remain open as long as the program is
+    # using the resource.
+    os.execvp(sys.argv[1], sys.argv[1:])
 
 if __name__ == '__main__':
     main()
