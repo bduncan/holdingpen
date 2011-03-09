9a0fd2d30223afcf68e316b8c70d76dcf480da1a
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-03-09 17:34:22 +0000

    Copy the init script to a place suitable for non-Debian.

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import sys
+import shutil
+import errno
+
+if sys.argv[1] == 'install':
+    try:
+        os.mkdir("scripts")
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise
+    shutil.copyfile("debian/holdingpen.init", "scripts/holdingpen")
+elif sys.argv[1] == 'clean':
+    try:
+        os.unlink("scripts/holdingpen")
+        os.rmdir("scripts")
+    except Exception:
+        pass
 
 setup(name="holdingpen",
       version="0.1",
       scripts=["holdingpen", "gatekeeper"],
       py_modules=["daemon"],
-      data_files=[('/etc/init.d', ['debian/holdingpen.init'])],
+      data_files=[('/etc/init.d', ['scripts/holdingpen'])],
       author="Bruce Duncan",
       author_email="Bruce.Duncan@ed.ac.uk",
       url="https://github.com/bduncan/holdingpen"
