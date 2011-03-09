45f6de79be2b2785c03fdd7bb2ffe2dffe16bb8b
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-03-09 01:11:10 +0000

    Add copyright/author/license notices.

--- gatekeeper.py
+++ gatekeeper.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 # vim: fileencoding=utf-8:
 
+"""Holding Pen Gatekeeper
+
+A wrapper script to request resources from the pen.
+
+Copyright (C) 2011 Bruce Duncan, University of Edinburgh
+
+    This file is part of Holding Pen.
+
+    Holding Pen is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Holding Pen is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+"""
+
 import os
 import sys
 import socket
