87963309cbeddf414c493413c47789a461640abf
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-03-09 17:16:37 +0000

    Add the init script to the packaging.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name="holdingpen",
       version="0.1",
       scripts=["holdingpen", "gatekeeper"],
       py_modules=["daemon"],
+      data_files=[('/etc/init.d', ['init.d/holdingpen'])],
       author="Bruce Duncan",
       author_email="Bruce.Duncan@ed.ac.uk",
       url="https://github.com/bduncan/holdingpen"
