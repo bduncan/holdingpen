9ff7900774e2abdf23f6c9a72f41fddbae9f5d67
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-03-09 16:00:17 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="holdingpen",
+      version="0.1",
+      py_modules=["holdingpen", "gatekeeper"],
+      author="Bruce Duncan",
+      author_email="Bruce.Duncan@ed.ac.uk",
+      url="https://github.com/bduncan/holdingpen"
+      )
