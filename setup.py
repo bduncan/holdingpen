368047688d5a5f4f76046deb1367ff183abf3198
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-03-09 16:37:51 +0000

    These programs are scripts, not modules.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name="holdingpen",
       version="0.1",
-      py_modules=["holdingpen", "gatekeeper"],
+      scripts=["holdingpen", "gatekeeper"],
       author="Bruce Duncan",
       author_email="Bruce.Duncan@ed.ac.uk",
       url="https://github.com/bduncan/holdingpen"
