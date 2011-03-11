f5a11aeec663c15cc997beec5aaf33693b47869a
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2011-03-11 14:59:23 +0000

    Fix git-buildpackage, making init script a copy
    debuild complains that setup.py cannot find scripts/holdingpen (the init
    script), even though it never uses it... Gah!
    
    Similarly, bdist_rpm complains that it can't find scripts/holdingpen
    because it was never picked up by sdist!

--- setup.py
+++ setup.py
@@ -7,26 +7,11 @@ import sys
 import shutil
 import errno
 
+
 def git_describe(abbrev=4):
     p = Popen(['git', 'describe', '--abbrev=%d' % abbrev], stdout=PIPE)
     return p.communicate()[0].split('\n')[0].strip()
 
-# distutils does not allow data_files to be renamed. Debian uses
-# debian/holdingpen.init but RPM wants /etc/init.d/holdingpen, so provide a
-# copy in scripts/
-if len(sys.argv) > 1 and sys.argv[1] in ('sdist', 'bdist', 'bdist_rpm'):
-    try:
-        os.mkdir("scripts")
-    except OSError, e:
-        if e.errno != errno.EEXIST:
-            raise
-    shutil.copyfile("debian/holdingpen.init", "scripts/holdingpen")
-elif len(sys.argv) > 1 and sys.argv[1] == 'clean':
-    try:
-        os.unlink("scripts/holdingpen")
-        os.rmdir("scripts")
-    except Exception:
-        pass
 
 setup(name="holdingpen",
       version=git_describe(),
